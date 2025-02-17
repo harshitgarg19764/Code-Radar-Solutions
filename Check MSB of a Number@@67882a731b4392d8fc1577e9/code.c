9f7e5a821c99ec41a6ea0409ce810a00400a7ff6
Harshit Garg <harshitgarg19764@gmail.com>
2025-02-18 02:47:15 +0530

    Solution to Check MSB of a Number

---
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+#include <stdio.h>
+int main(){
+    int num;
+    int binary;
+    int ans=0;
+    scanf("%d",&num);
+    while(num>0){
+        int binary = num%2;
+        // ans = ans*10 + binary;
+        num=num/2; 
+    }
+        printf("%d",binary);
+}
